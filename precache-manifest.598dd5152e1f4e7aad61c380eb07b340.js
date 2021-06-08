77d84ab147f642e658a4027fd1faad287552b76f
Fran Swan <71414784+franswan@users.noreply.github.com>
2021-06-08 06:12:52 -0700

    send it

---
+++ precache-manifest.598dd5152e1f4e7aad61c380eb07b340.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '9cf493c84293c4371c7f2749a6b27d15',
+    url: '/index.html',
+  },
+  {
+    revision: '99c2e68842e4c62043d2',
+    url: '/static/css/2.f7eab022.chunk.css',
+  },
+  {
+    revision: '3b824fbe27d718562332',
+    url: '/static/css/main.29ef57e7.chunk.css',
+  },
+  {
+    revision: '99c2e68842e4c62043d2',
+    url: '/static/js/2.b993cc2e.chunk.js',
+  },
+  {
+    revision: 'd2e09aded9686c343e34a065031febca',
+    url: '/static/js/2.b993cc2e.chunk.js.LICENSE.txt',
+  },
+  {
+    revision: '3b824fbe27d718562332',
+    url: '/static/js/main.6f68c83c.chunk.js',
+  },
+  {
+    revision: 'd3ebb56a3098223b5316',
+    url: '/static/js/runtime-main.c7319a82.js',
+  },
+  {
+    revision: '7402247c9677583223b4a9f09941e3ff',
+    url: '/static/media/logo.7402247c.gif',
+  },
+]);
